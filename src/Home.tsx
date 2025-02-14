f392746120079dc8c6a091df0a4bcde53cb4141f
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-02-13 23:02:21 -0500

    free recording session

--- src/Home.tsx
+++ src/Home.tsx
@@ -321,9 +321,9 @@ const Home = () => {
         </Row>
 
 
-        <Row style={rowStyle}>
-            <div style={{margin:"0 auto", height:"100%"}}>
-              <iframe width="540" height="305" src="https://8b7144f1.sibforms.com/serve/MUIFAHJjCbANvXSbNqPtNOOWw3Ni0pOj0791GG55dqyhe48lQtKa0M5GR25MfCEasLuIu9tgEkd8ORvkMLe7pC0GZnLhtqKZ4d6UkiGcpFt7UZ2xK_H9lFCdZhQbwLJgNLVpQ79mQMC4zfPowXrulOrMQ5-MGkFk9-zRVao5Busdb_N502Z8w3DlLXaZ4z0w6XUP98tJudjAfKb7" frameBorder="0" scrolling="auto" style={{display: "block" ,marginLeft: "auto", marginRight: "auto" ,maxWidth: "100%"}}></iframe>
+        <Row style={{margin:"0 auto", height:"100%"}}>
+            <div>
+              <iframe width="540" height="100%" src="https://8b7144f1.sibforms.com/serve/MUIFAHJjCbANvXSbNqPtNOOWw3Ni0pOj0791GG55dqyhe48lQtKa0M5GR25MfCEasLuIu9tgEkd8ORvkMLe7pC0GZnLhtqKZ4d6UkiGcpFt7UZ2xK_H9lFCdZhQbwLJgNLVpQ79mQMC4zfPowXrulOrMQ5-MGkFk9-zRVao5Busdb_N502Z8w3DlLXaZ4z0w6XUP98tJudjAfKb7" frameBorder="0" scrolling="auto" style={{display: "block" ,marginLeft: "auto", marginRight: "auto" ,maxWidth: "100%"}}></iframe>
             </div>
           </Row>
 
