053274f850762140c75ea294ccbc963b1cdbfb4e
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-02-13 22:36:49 -0500

    free recording session

--- src/Home.tsx
+++ src/Home.tsx
@@ -322,7 +322,7 @@ const Home = () => {
 
        
         <Row style={servicesStyle}>
-          <Row >
+          <Row style={rowStyle}>
             <div style={{margin:"0 auto", height:"100%"}}>
               <iframe width="540" height="305" src="https://8b7144f1.sibforms.com/serve/MUIFAHJjCbANvXSbNqPtNOOWw3Ni0pOj0791GG55dqyhe48lQtKa0M5GR25MfCEasLuIu9tgEkd8ORvkMLe7pC0GZnLhtqKZ4d6UkiGcpFt7UZ2xK_H9lFCdZhQbwLJgNLVpQ79mQMC4zfPowXrulOrMQ5-MGkFk9-zRVao5Busdb_N502Z8w3DlLXaZ4z0w6XUP98tJudjAfKb7" frameBorder="0" scrolling="auto" style={{display: "block" ,marginLeft: "auto", marginRight: "auto" ,maxWidth: "100%"}}></iframe>
             </div>
